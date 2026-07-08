0ffd9fd4c1d94f112e4efa8c634717bd6b7c8ddd
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-07-08 14:10:53 +0300

    created a user model and a refresh token model created base model

---
+++ app/modules/auth/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, ForeignKey
+from datetime import datetime
+
+import enum
+from app.database import Base
+
+
+class UserRole(str, enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(255))
+    role: Mapped[UserRole] = mapped_column(default=UserRole.USER)
+
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    token: Mapped[str] = mapped_column(String(512), unique=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), ondelete="CASCADE")
+    expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
