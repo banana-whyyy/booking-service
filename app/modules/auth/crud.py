c5f4209423360c68e9b134dc444727943ca5862c
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-07-09 16:30:35 +0300

    added functions for working with refresh tokens

--- app/modules/auth/crud.py
+++ app/modules/auth/crud.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from datetime import UTC, datetime, timedelta
 
-from app.modules.auth.models import User
+from app.modules.auth.models import User, RefreshToken
 from app.modules.auth.schemas import UserCreate
 from app.modules.auth.security import get_password_hash
+from app.config import settings
 
 
 async def get_user(db: AsyncSession, user_id: int) -> User | None:
@@ -38,3 +40,24 @@ async def create_user(db: AsyncSession, user: UserCreate) -> User:
     await db.flush()
     await db.refresh(db_user)
     return db_user
+
+
+async def register_refresh_token_db(db: AsyncSession, token: str, user_id: int) -> RefreshToken:
+    expire_at = datetime(UTC) + timedelta(days=settings.refresh_token_expire_days)
+    db_refresh = RefreshToken(
+        user_id=user_id,
+        expires_at=expire_at,
+        token=token
+    )
+
+    db.add(db_refresh)
+    await db.flush()
+    await db.refresh(db_refresh)
+    return db_refresh
+
+
+async def get_refresh_token(db: AsyncSession, token: str) -> RefreshToken | None:
+    result = await db.execute(
+        select(RefreshToken).where(RefreshToken.token == token).with_for_update()
+    )
+    return result.scalar_one_or_none()
