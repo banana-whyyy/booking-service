1207f7f9ba6067caa8b700635ad93aff82bda6ea
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-07-09 12:36:40 +0300

    set up async session

--- app/database.py
+++ app/database.py
@@ -1,5 +1,18 @@
 from sqlalchemy.orm import DeclarativeBase
+from collections.abc import AsyncGenerator
+from app.config import settings
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 
 
 class Base(DeclarativeBase):
     pass
+
+
+# в проде поменять на echo=False
+engine = create_async_engine(settings.database_url, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:        
+        yield session
