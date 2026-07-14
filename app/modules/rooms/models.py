41c542bccdd23a4f8f51356a32874d4617e868ed
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-07-14 14:43:24 +0300

    changed price_hour and created_at in /rooms/models to a more valid data type

--- app/modules/rooms/models.py
+++ app/modules/rooms/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Text, String
+from sqlalchemy import Text, String, DateTime, func, Numeric
 from datetime import datetime
+from decimal import Decimal
 
 from app.database import Base
 
@@ -11,9 +12,9 @@ class Rooms(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String, index=True)
     description: Mapped[str | None] = mapped_column(Text)
-    price_hour: Mapped[int]
+    price_hour: Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2))
     capacity: Mapped[int] = mapped_column(default=5)
     has_projector: Mapped[bool]
     has_whiteboard: Mapped[bool]
-    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
