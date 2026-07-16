f10db6d20f042fafa18df333a2f195c91c7db2bd
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-07-16 14:38:41 +0300

    moved dependencies from auth to the project root also fixed the DRY behavior

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,25 +1,21 @@
 from fastapi import Depends, HTTPException, status
 from jose import JWTError
 from fastapi.security import OAuth2PasswordBearer 
-from typing import Annotated
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database import engine, get_db
-from app.config import settings
-from auth.models import User 
-from auth.security import decode_jwt 
+from .database import get_db
+from modules.auth.models import User 
+from modules.auth.crud import get_user
+from modules.auth.security import decode_jwt 
 
 
 reusable_oath2 = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-SessionDep = Annotated[AsyncSession, Depends(get_db)]
-TokenDep = Annotated[str, Depends(reusable_oath2)]
 
-async def get_current_user(session: SessionDep, token: TokenDep) -> User:
+async def get_current_user(session: AsyncSession = Depends(get_db), token: str = Depends(reusable_oath2)) -> User:
     cred_exceptions = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token type"
+            detail="Could not validate credentials"
         )
     
     try:
@@ -28,13 +24,10 @@ async def get_current_user(session: SessionDep, token: TokenDep) -> User:
         token_type = payload["type"]
         if token_type != "access":
             raise cred_exceptions
-    except (JWTError, ValueError):
+    except (JWTError, ValueError, KeyError):
         raise cred_exceptions
     
-    result = await session.execute(
-        select(User).where(User.id == int(user_id))
-    )
-    user = result.scalars().first()
+    user = await get_user(session, user_id=int(user_id))
 
     if user is None: 
         raise cred_exceptions
