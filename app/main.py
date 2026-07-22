bf5d53b2296f25c59ef9af1b9ed2f26f2668b433
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-07-22 13:36:53 +0300

    added routes for rooms in main fix import at the entry point

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 
-from app.modules.auth.router import auth
+from app.modules.auth.router import router as auth_router
+from app.modules.rooms.router import router as rooms_router
+
 
 app = FastAPI(
     title="Booking Service",
     description="Service for finding meeting rooms",
 )
 
-app.include_router(auth)
+app.include_router(auth_router)
+app.include_router(rooms_router)
 
 @app.get("/")
 async def root():
@@ -15,7 +18,8 @@ async def root():
         "message": "Booking service",
         "docs": "/docs",
         "endpoints": {
-            "auth": "/auth/register, /auth/login, /auth/refresh"
+            "auth": "/auth/register, /auth/login, /auth/refresh",
+            "rooms": "/rooms",
             }
         }
 
