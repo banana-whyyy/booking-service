db4a875d6c831e9fb3ef9e06d2f63206f9966fc6
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-08-17 14:49:53 +0300

    fix: ticket generaion

--- app/modules/notifications/pdf.py
+++ app/modules/notifications/pdf.py
@@ -1,18 +1,43 @@
 import io
+from datetime import datetime
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
-def generate_ticket_pdf(booking_id: int, user_email: str, room_name: str) -> bytes:
+def generate_ticket_pdf(
+    booking_id: int, 
+    username: str, 
+    room_name: str,
+    time_start: str | datetime,
+    time_end: str | datetime
+) -> bytes:
     buffer = io.BytesIO()
     p = canvas.Canvas(buffer, pagesize=letter)
 
+    if isinstance(time_start, str):
+        time_start = datetime.fromisoformat(time_start)
+    if isinstance(time_end, str):
+        time_end = datetime.fromisoformat(time_end)
+
+    formatted_date = time_start.strftime("%Y-%m-%d")
+    formatted_time = f"{time_start.strftime('%H:%M')} - {time_end.strftime('%H:%M')}"
+
     p.setFont("Helvetica-Bold", 18)
-    p.drawString(100, 750, "Бронирование подтверждено!")
+    p.setFillColor(colors.HexColor("#2C3E50"))
+    p.drawString(100, 750, "Booking Confirmed!")
+
+    p.setStrokeColor(colors.HexColor("#BDC3C7"))
+    p.setLineWidth(1)
+    p.line(100, 735, 500, 735)
 
     p.setFont("Helvetica", 12)
-    p.drawString(100, 710, f"ID Брони: #{booking_id}")
-    p.drawString(100, 690, f"Пользователь: {user_email}")
-    p.drawString(100, 670, f"Переговорка: {room_name}")
+    p.setFillColor(colors.black)
+    
+    p.drawString(100, 700, f"Booking ID: #{booking_id}")
+    p.drawString(100, 680, f"User: {username}")
+    p.drawString(100, 660, f"Room: {room_name}")
+    p.drawString(100, 640, f"Date: {formatted_date}")
+    p.drawString(100, 620, f"Time: {formatted_time}")
 
     p.showPage()
     p.save()
