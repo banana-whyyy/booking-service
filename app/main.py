9bb4dafafb1ec2eba6bf4e32183dc654a384fc73
Yuriy Medvedev <yuriy.medvedev.33@mail.ru>
2026-08-24 14:24:48 +0300

    feat: use local swagger static files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from app.middlewares import register_middlewares
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.docs import get_swagger_ui_html
 
+from app.middlewares import register_middlewares
 from app.modules.auth.router import router as auth_router
 from app.modules.rooms.router import router as rooms_router
 from app.modules.bookings.router import router as bookings_router
@@ -9,7 +11,12 @@ from app.modules.bookings.router import router as bookings_router
 app = FastAPI(
     title="Booking Service",
     description="Service for finding meeting rooms",
+    docs_url=None,
+    redoc_url=None,
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 # В проде поменять middleware allow_origins 
 register_middlewares(app)
 
@@ -29,3 +36,13 @@ async def root():
             }
         }
 
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=f"{app.title} - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger/swagger-ui.css",
+        swagger_favicon_url="/static/swagger/favicon.png",
+    )
